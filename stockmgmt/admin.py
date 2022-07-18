1daae29494ce7a67eb9825ab48aead6433417920
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2022-07-18 19:29:16 +0530

    ...

--- stockmgmt/admin.py
+++ stockmgmt/admin.py
@@ -6,6 +6,8 @@ from .forms import StockCreateForm
 class StockCreateAdmin(admin.ModelAdmin):
     list_display = ['category', 'item_name', 'quantity']
     form = StockCreateForm
+    list_filter = ['category']
+    search_fields = ['category', 'item_name']
 
 
 admin.site.register(Stock, StockCreateAdmin)
--- stockmgmt/views.py
+++ stockmgmt/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import Stock
+from .forms import StockCreateForm
 
-# Create your views here.
+
+def home(request):
+    title = 'Welcome'
+    context = {
+        "title": title,
+    }
+    return render(request, 'stockmgmt/home.html', context)
+
+
+def list_items(request):
+    title = 'List of Items'
+    queryset = Stock.objects.all()
+    context = {
+        "title": title,
+        "queryset": queryset,
+    }
+    return render(request, 'stockmgmt/listitems.html', context)
+
+
+def add_items(request):
+    form = StockCreateForm(request.POST)
+    if form.is_valid():
+        form.save()
+    context = {
+        "form": form,
+        "title": "Add Item",
+    }
+    return render(request, 'stockmgmt/additems.html', context)
+
