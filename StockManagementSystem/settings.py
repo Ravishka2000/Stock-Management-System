0d583e2d0e8bbae36bd2e6db826221c53ca51c79
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2022-07-20 19:17:34 +0530

    Done and dusted

--- StockManagementSystem/settings.py
+++ StockManagementSystem/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'stockmgmt',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
